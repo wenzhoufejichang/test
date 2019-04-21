6f64673d049f8e9e4535697958f7f6dcad2761b4
hzm <771746364@qq.com>
2019-04-21 21:56:25 +0800

    随意修改

--- D2/src/com/hzm/A1.java
+++ D2/src/com/hzm/A1.java
@@ -2,34 +2,28 @@ package com.hzm;
 
 public class A1 {
 	public static void main(String[] args) {
-		
-		int aaa=1,b=2;
-		
-		System.out.println(aaa+b+"");
-//		A a = new A();
-//
-//		new Thread(a).start();
-//
-//		while (true)
-//			if ("2".equals(a.getSs())) {
-//				System.out.println("----");
-//				break;
-//
-//			}
 
-		
-		
-		
-		
-		
-		
-		
+		int aaa = 1, b = 2;
+		int a = 1, b2 = 2;
+
+		System.out.println(aaa + b + "");
+		// A a = new A();
+		//
+		// new Thread(a).start();
+		//
+		// while (true)
+		// if ("2".equals(a.getSs())) {
+		// System.out.println("----");
+		// break;
+		//
+		// }
+
 	}
 }
 
 class A implements Runnable {
 
-	private  String ss = "1";
+	private String ss = "1";
 
 	@Override
 	public void run() {
